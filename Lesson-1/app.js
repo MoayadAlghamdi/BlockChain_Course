f0a23498e2a931bd6005d72f173fae2e75213fc6
MDFCIT <mdfcit.16@gmail.com>
2023-01-18 15:48:57 +0300

    Add Validation and mining for blockchain

--- Lesson-1/app.js
+++ Lesson-1/app.js
@@ -1,72 +1,104 @@
 const { createHash } = require('crypto');
 
 class Block{
-    Index
-    Time_Stamp
-    Previous_Hash
-    Hash
-    Data
-    Nonce
-    Height
-
-    constructor(data,previous_hash,nonce,height){
+    constructor(data){
+        this.Index = 0;
+        this.Time_Stamp = new Date();
         this.Data = data;
         this.Hash = this.HashFunction();
-        this.Previous_Hash = previous_hash;
-        this.Nonce = nonce;
-        this.Height = height;
-        this.Time_Stamp = new Date();
-        this.Index = 1;
+        this.Previous_Hash = ""; // previous block's hash
+        this.Nonce = 0; // set nonce zero and incrementing in mine function over and over again until the hash matches the one we need.
+        this.Height = 0;
     }
      HashFunction() {
+        // return the hash that contain from 
         return createHash('sha256').update(this.Index+this.Nonce+this.Data+this.Previce_Hash).digest('hex');
+    }
+
+    mine(difficulty) {
+        // Basically, it loops until our hash starts with 0...000 with length of <difficulty>.
+        while (!this.Hash.startsWith(Array(difficulty + 1).join("0"))) {
+            // We increases our nonce so that we can get a whole different hash.
+            this.Nonce++;
+            // Update our new hash with the new nonce value.
+            this.Hash = this.HashFunction();
+        }
     }
     
 }
 
 class BlockChain {
-    ListArray = [];
-    constructor(data,previous_hash,nonce,height){
-        if(this.ListArray.length == 0){
-        this.ListArray.push(new Block(data,previous_hash,0,0));
+    chain = [];
+    difficulty = 1;
+    constructor(data){
+        if(this.chain.length == 0){
+        this.chain.push(new Block(data));
         }
     }
-    addBlock(data,nonce,height){
-        this.ListArray.push(new Block(data,this.ListArray[this.ListArray.length-1].Previous_Hash,nonce,height));
-        }
+
+    addBlock(block){
+        block.Index = this.getLastBlock().Index+1;
+        block.Previous_Hash = this.getLastBlock().Hash;
+        block.Hash = block.HashFunction();
+        block.Height = this.getLastBlock().Height+1;
+        block.mine(this.difficulty);
+        this.chain.push(block);
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
 
     print(){
-        return this.ListArray;
-    }
-
-    validBlockChain(){
-        for(var i = 1 ; i < this.ListArray.length ; i++){
-            if(this.ListArray[i].Previous_Hash != this.ListArray[i-1].Previous_Hash){
-                return "Invalid";
+        return this.chain;
+    }   
+    // check here if the blockchain does not change at any block
+    isBlockChainValid(){
+        this.Found = "Invalid";
+        for(var i = 1 ; i < this.chain.length ; i++){
+            if(this.chain[i].Previous_Hash != this.chain[i-1].Previous_Hash){
+                this.Found = "Valid";
             }
         }
-        return "Valid";
+        return this.Found;
     }
-
-    validBlockChain(){
-        for(var i = 1 ; i < this.ListArray.length ; i++){
-            if(this.ListArray[i].Previous_Hash != this.ListArray[i-1].Previous_Hash){
-                return "Invalid";
+    // check here if the block does not change
+    isBlockValid(){
+        this.Found = "Invalid";
+        for(var i = 1 ; i < this.chain.length ; i++){
+            if(this.chain[i].Previous_Hash != this.chain[i-1].Previous_Hash){
+                this.Found = "Valid";
             }
         }
-        return "Valid";
+        return this.Found;
     }
     
+     // get the current height of each block and get the height of blockchain
     getCurrentBlcokHeight(){
-        for(var i = 0 ; i < this.ListArray.length ; i++){
-            console.log("Each Height Block: "+this.ListArray[i].Height);
+        for(var i = 0 ; i < this.chain.length ; i++){
+            console.log("Block Index("+i+"): "+this.chain[i].Height);
         }
-        console.log("The Height of the Block: "+this.ListArray.length);
+        console.log("\nThe Height of the Block: "+this.chain.length);
     }
     
 }
-var blockChain = new BlockChain("guiness","N/A",0,0);
-console.log(blockChain.addBlock("Saleh -> Moayad 1.5 BTC",4059,677350));
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+var blockChain = new BlockChain("Genesis");
+for(var i = 0 ; i < 7 ; i++){
+var randomText = makeid(35)
+blockChain.addBlock(new Block(randomText));
+}
 console.log(blockChain.print());
+console.log("");
 blockChain.getCurrentBlcokHeight();
-console.log(blockChain.validBlockChain())
+console.log("\nThe BlockChain is : "+blockChain.isBlockChainValid());
