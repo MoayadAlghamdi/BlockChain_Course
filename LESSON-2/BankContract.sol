b4513ae3fb8a72d592cd10b8eff5eecf981ec873
MDFCIT <mdfcit.16@gmail.com>
2023-01-23 20:56:15 +0300

    Create Bank Contract by Solidity

---
+++ LESSON-2/BankContract.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.22 <0.9.0;
+contract Counter{
+    
+    struct User{
+        uint id;
+        address userAddress;
+        UserType userType;
+    }
+        mapping (address => User) public accounts;
+        address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        mapping (address => uint256) public balances;
+        mapping (address => bool) public isAdminType;
+        uint256 public counter;
+
+    enum UserType{
+        Admin,
+        User
+    }
+    
+    constructor() {
+        User memory user = User(0,msg.sender,UserType.Admin);
+        accounts[msg.sender] = user;
+        isAdminType[msg.sender] = true;
+    }
+
+    function register() public{
+        User memory user = User(counter,msg.sender,UserType.User);
+        accounts[msg.sender] = user;
+        counter++;
+    }
+    function makeAdmin(address _address) public{
+        User memory user = User(counter,_address,UserType.Admin);
+        accounts[msg.sender] = user;
+        isAdminType[msg.sender] = true;
+        counter++;
+    }
+
+    function promoto() public{
+    require(isAdminType[msg.sender] == true, "Ownership Assertion: Caller of the function is not the owner.");
+      
+    }
+
+    function depositMoney() public payable{
+          balances[msg.sender] += msg.value;
+    }
+    function withDrowMoney(address _address , uint amount) public view returns(uint) {
+        require(msg.sender == _address ,"You don't have authraization");
+        require(balances[msg.sender]>= amount, "Not enough ether");
+        uint balance = address(this).balance;
+        balance -= amount;
+        return balance ;
+    }
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+}
